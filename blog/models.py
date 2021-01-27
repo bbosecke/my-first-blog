997638490f436e7fd078657dd5476e5488fb9ccd
bbosecke <brodie.bosecke.yt@gmail.com>
2021-01-28 10:39:06 +1300

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+#defining an object
+#models.Model means that the Post is a Django MOdel, so Django knows...
+#... that it should be stored in the database
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published.date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
